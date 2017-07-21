9fe8578e26c1fb7c40a56784403ef2697126abcd
creen9 <levsk8moto@yahoo.com>
2017-07-21 19:39:47 +0200

    edited krogi

--- generators.lua
+++ generators.lua
@@ -6,9 +6,9 @@ function generate_circles( t, numseg, starting, retries, exDist )
 		-- If they colide, remove the last table entry
 		for j=1,i-1 do
 			-- preveri le za tiste kroge, ki hkrati niso sosedi
-			if not has_value( t[i][4], {j} ) then
+			if not has_value( t[i].ngs, {j} ) then
 			-- if true then
-				if difft(t, i, j) < t[i][3] + t[j][3]+exDist then
+				if difft(t, i, j) < t[i].r + t[j].r+exDist then
 					if retries > 10 then
 						for _=1,5 do
 							table.remove(t)
@@ -30,11 +30,9 @@ end
 -- generira enemy-je
 function generate_npcs( t, Pnpc, minR )
 	local newBonus = 30
-	table.insert(t[1], {})
 	for i=2,numSeg do
-		table.insert(t[i], {})
 		if m.random() < Pnpc and
-				t[i][3] < minR then
+				t[i].r < minR then
 			local inout = m.random(-1, 1)
 			if inout <= 0 then
 				inout = -1
@@ -46,10 +44,10 @@ function generate_npcs( t, Pnpc, minR )
 			local ypos
 			repeat
 				 randFi = m.random( -m.pi, m.pi )
-				 xpos = t[i][1] + (t[i][3] - Rnpc*inout)*m.cos(randFi)
-				 ypos = t[i][2] + (t[i][3] - Rnpc*inout)*m.sin(randFi)
-			until farEnough( t, t[i][4], {xpos, ypos}, newBonus )
-			table.insert(t[i][5], { xpos, ypos })
+				 xpos = t[i].x + (t[i].r - Rnpc*inout)*m.cos(randFi)
+				 ypos = t[i].y + (t[i].r - Rnpc*inout)*m.sin(randFi)
+			until farEnough( t, t[i].ngs, {xpos, ypos}, newBonus )
+			table.insert(t[i].npcs, { xpos, ypos })
 		end
 	end
 	return t
@@ -62,11 +60,11 @@ function genone( t )
 	local randFi = m.random( -dfid*100, dfid*100 )/100
 	local newFi = preFi + randFi
 	local randR = m.random( rmin, rmax )
-	local newX = t[L][1] + ( randR + t[L][3] )*m.cos(newFi)
-	local newY = t[L][2] + ( randR + t[L][3] )*m.sin(newFi)
-	local newCircle = { newX, newY, randR, {L} }
+	local newX = t[L].x + ( randR + t[L].r )*m.cos(newFi)
+	local newY = t[L].y + ( randR + t[L].r )*m.sin(newFi)
+	local newCircle = { x = newX, y = newY, r = randR, ngs = {L}, npcs = {} }
 
-	table.insert(t[L][4], L+1)
+	table.insert(t[L].ngs, L+1)
 	table.insert(t, newCircle)
 
 	return t
