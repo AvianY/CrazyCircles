f2288aa1ece225247bc4d2a8a33754f76ffd55f2
aviany <ayelite@gmail.com>
2017-08-07 22:55:44 +0200

    Added support for genfour. It works :)

--- generators.lua
+++ generators.lua
@@ -56,12 +56,19 @@ end
 --	-Brez while loopa
 --	-For loop zazeljeno predstavlja stevilo krogov
 --	-Brez preverjanja kolizij med generiranjem.
-function generate_circles1( t, numseg, exDist )
-	for i=1,numseg do
-		t = genone_col( t, exDist )
-		-- if i ~= (#t - 2) then
-		-- 	print(i, #t-2)
-		-- end
+function generate_circles1( t, numKrog, exDist )
+	local i = 2
+	while i <= numKrog do
+		if m.random() < 0.8 then
+			t = genone_col( t, exDist )
+			i = i + 1
+		else
+			t = genfour_col( t, exDist )
+			i = i + 4
+		end
+		if i ~= (#t) then
+			print(i, #t)
+		end
 	end
 end
 
@@ -143,7 +150,7 @@ end
 -- poskusil še enkrat. Krog bo generiral glede na povprečje ostalih
 -- krogov.
 function genone_col( t, exDist)
-	local newCircle = {}
+	local newCircle
 	repeat
 		local avgFi = avgAngle( t )
 		local randFi = m.random( -krg.dfid*100, krg.dfid*100 )/100
@@ -156,6 +163,77 @@ function genone_col( t, exDist)
 
 	table.insert(t[#t].ngs, #t+1)
 	table.insert(t, newCircle)
+
+	return t
+end
+
+-- generira 4 kroge in jih doda v 't'. Če se križajo z drugimi krogi, bo
+-- poskusil še enkrat. Kroge bo generiral glede na povprečje ostalih
+-- krogov.
+function genfour_col( t, exDist)
+	
+	local newCircle1
+	local newCircle2
+	local newCircle3
+	local newCircle4
+
+	repeat
+		local avgFi = avgAngle( t )
+		local preR = t[#t].r
+		-- Iz nakljucnega ter prejsnjega R-ja izracunamo fi
+		local randR = m.random( krg.rmin, krg.rmax )
+		local clusFi = m.asin( randR / ( randR + preR) )
+		local randn = m.random(1,2)
+		if randn == 2 then
+			-- kroga v clusterju se ne stikata
+			local randkot = m.random(10, 30) / 100
+			clusFi = clusFi + randkot
+		end
+		local clus1X = t[#t].x + ( randR + preR )*m.cos(avgFi + clusFi)
+		local clus1Y = t[#t].y + ( randR + preR )*m.sin(avgFi + clusFi)
+
+		local clus2X = t[#t].x + ( randR + preR )*m.cos(avgFi - clusFi)
+		local clus2Y = t[#t].y + ( randR + preR )*m.sin(avgFi - clusFi)
+
+		-- nakljucen R za zakljucitveni krog za gruco (cluster)
+		local randR1 = m.random( krg.rmin, krg.rmax )
+
+		local height1, height2
+		if randn == 1 then
+			height1 = m.sqrt( (preR + randR)^2 - (randR)^2 )
+			height2 = m.sqrt( (randR1 + randR)^2 - (randR)^2 )
+		else
+			local extRandR = m.sin(clusFi)*(randR + preR)
+			height1 = m.sqrt( (preR + randR)^2 - (extRandR)^2 )
+			height2 = m.sqrt( (randR1 + randR)^2 - (extRandR)^2 )
+		end
+		
+		local zakkrg1X = t[#t].x + ( height1 + height2 )*m.cos(avgFi)
+		local zakkrg1Y = t[#t].y + ( height1 + height2 )*m.sin(avgFi)
+		
+		-- nakljucen R za zakljucitveni krog celotne stvari (cluster)
+		local randR2 = m.random( krg.rmin, krg.rmax )
+		local randFi2 = m.random( -krg.dfid*100, krg.dfid*100 )/100
+		local zakkrg2X = zakkrg1X + ( randR1 + randR2 )*m.cos(avgFi + randFi2)
+		local zakkrg2Y = zakkrg1Y + ( randR1 + randR2 )*m.sin(avgFi + randFi2)
+
+		newCircle1 = { x = clus1X, y = clus1Y, r = randR, ngs = {#t, #t+2, #t+3}, npcs = {}, pts = {} }
+		newCircle2 = { x = clus2X, y = clus2Y, r = randR, ngs = {#t, #t+1, #t+3}, npcs = {}, pts = {} }
+		newCircle3 = { x = zakkrg1X, y = zakkrg1Y, r = randR1, ngs = {#t+1, #t+2 , #t+4}, npcs = {}, pts = {} }
+		newCircle4 = { x = zakkrg2X, y = zakkrg2Y, r = randR2, ngs = { #t+3 }, npcs = {}, pts = {} }
+
+	until checkCollisions( t , {{ x = newCircle1.x, y = newCircle1.y, r = newCircle1.r},
+								{ x = newCircle2.x, y = newCircle2.y, r = newCircle2.r},
+								{ x = newCircle3.x, y = newCircle3.y, r = newCircle3.r},
+								{ x = newCircle4.x, y = newCircle4.y, r = newCircle4.r} }, exDist  )
+
+	table.insert(t[#t].ngs, #t+1)
+	table.insert(t[#t].ngs, #t+2)
+
+	table.insert(t, newCircle1)
+	table.insert(t, newCircle2)
+	table.insert(t, newCircle3)
+	table.insert(t, newCircle4)
 
 	return t
 end
